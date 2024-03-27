600ac156fec1b6683cde69be8dfd443100e4e012
Kakune55 <xiao_doubi9637@outlook.com>
2024-03-27 22:45:27 +0800

    优化输出排版 添加快速测试功能

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time, csv, tqdm, dns.resolver
 
-domain = "python.org"
-testRepeat = 5 #必须大于等于1 且为整数
+domain = "baidu.com"
+testRepeat = 10 #必须大于等于1 且为整数
 csvPath = "dnslist.csv"
 
 def delayTest(nameserver:str,repeat:int):
@@ -15,33 +15,51 @@ def delayTest(nameserver:str,repeat:int):
             end = time.time()
         except:
             return 9999
-        alltime += end - start 
+        alltime += end - start
     return alltime/repeat*1000
-try:
-    csv_reader = csv.reader(open(csvPath,encoding='utf-8'))
-except:
-    print("读取dns列表失败")
+
+def runTest(csvPath:str,repeat:int):
+    try:
+        csv_reader = csv.reader(open(csvPath,encoding='utf-8'))
+    except:
+        print("读取dns列表失败")
+        input()
+        exit()
+    delayTable = []
+    with tqdm.tqdm(total=len(open(csvPath,encoding='utf-8').readlines())) as pbar:
+        for row in csv_reader:
+            for i in range(len(row)):
+                if i == 0:
+                    pbar.desc = row[0]
+                elif row[i] == "":
+                    continue
+                else:
+                    delay = delayTest(row[i],repeat)
+                    delayTable.append((row[0] + " " + row[i], delay))
+            pbar.update(1)
+
+    print("结果如下\n")
+    for i in sorted(delayTable, key=lambda x:x[1]): #格式化输出
+        if i[1] < 100:
+            print(f"{format(i[1],'.2f')}ms\t{i[0]}")
+        elif i[1] < 1000:
+            print(f"{format(i[1],'.1f')}ms\t{i[0]}")
+        else:
+            print(f"{format(i[1],'.0f')} ms\t{i[0]}")
+
+if __name__ == '__main__':
+    menuSwitch = input("1.快速测试 2.平均值测试")
+    if menuSwitch == "1":
+        print(f"开始快速测试")
+        runTest(csvPath, 1)
+    elif menuSwitch == "2":
+        print(f"开始测试 循环次数：{testRepeat}")
+        runTest(csvPath, testRepeat)
+    else:
+        print("未知选项")
     input()
     exit()
+    
+    
 
-delayTable = []
-with tqdm.tqdm(total=len(open(csvPath,encoding='utf-8').readlines())) as pbar:
-    for row in csv_reader:
-        for i in range(len(row)):
-            if i == 0:
-#                print("\n",row[0])
-                pbar.desc = row[0]
-            elif row[i] == "":
-                continue
-            else:
-#                print(row[i],end="\t\t")
-                delay = delayTest(row[i],5)
-#                print(format(delay,'.4f'),"ms")
-                delayTable.append((row[0] + " " + row[i], delay))
-        pbar.update(1)
-
-print("---------------")
-print("MinDelayNameServer is")
-for i in sorted(delayTable, key=lambda x:x[1]):
-    print(f"{i[0]} {format(i[1],'.2f')}ms")
-input()
+    
