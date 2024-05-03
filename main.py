c6f27a8ba63441131e2498538ab52d13db848187
Kakune55 <xiao_doubi9637@outlook.com>
2024-05-03 20:40:11 +0800

    feat:添加在线获取dns列表功能

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time, csv, tqdm, dns.resolver
+import time, csv, tqdm, dns.resolver, requests
 
+csv_url = "https://github.com/Kakune55/DNSspeedtest/releases/download/v1.1.0/dnslist.csv"
 domain = "baidu.com"
 testRepeat = 10 #必须大于等于1 且为整数
 csvPath = "dnslist.csv"
@@ -18,13 +19,14 @@ def delayTest(nameserver:str,repeat:int):
         alltime += end - start
     return alltime/repeat*1000
 
+
+def localRead():
+    return csv.reader(open(csvPath,encoding='utf-8'))
+
+def OnlineRead():
+    return csv.reader(requests.get(csv_url).content.decode('utf-8').splitlines())
+
 def runTest(csvPath:str,repeat:int):
-    try:
-        csv_reader = csv.reader(open(csvPath,encoding='utf-8'))
-    except:
-        print("读取dns列表失败")
-        input()
-        exit()
     delayTable = []
     with tqdm.tqdm(total=len(open(csvPath,encoding='utf-8').readlines())) as pbar:
         for row in csv_reader:
@@ -48,6 +50,22 @@ def runTest(csvPath:str,repeat:int):
             print(f"{format(i[1],'.0f')} ms\t{i[0]}")
 
 if __name__ == '__main__':
+    
+    try:
+        print("尝试从在线加载DNS列表",end="--")
+        csv_reader = OnlineRead()
+        print("成功")
+    except:
+        print("失败")
+        try:
+            print("尝试从本地加载DNS列表",end="--")
+            csv_reader = localRead()
+            print("成功")
+        except:
+            print("失败")
+            input()
+            exit()
+    
     menuSwitch = input("1.快速测试 2.平均值测试")
     if menuSwitch == "1":
         print(f"开始快速测试")
@@ -59,7 +77,4 @@ if __name__ == '__main__':
         print("未知选项")
     input()
     exit()
-    
-    
-
     
